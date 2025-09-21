90ce6bca0909f472e73ca219bf6c7f7e0c8f6d3d
Mantaj-GitSub <mantajsidhu1119@gmail.com>
2025-09-21 23:04:35 +0530

    Project-ATM.py

--- Project-ATM.py
+++ Project-ATM.py
@@ -9,26 +9,26 @@ Kindly Enter Your Choice:
       """)
 choice = int(input("Enter Your Choice: "))
 
-if 1<=choice<=4 :
+if (1<=choice<=4) :
     while 1<=choice<=4:
        
-        if choice == 1:
+        if (choice == 1):
             print("Your balance is: {:,.2f}".format(Balance))
             
-        elif choice == 2:
+        elif (choice == 2):
             dpamt = int(input("Enter Deposit Amount: "))
             Balance += dpamt
             print("Deposit Successful. Your New Balance is {:,.2f} ".format(Balance))
 
-        elif choice == 3:
+        elif (choice == 3):
             wdamt = int(input("Enter Withdrawal Amount: "))
-            if wdamt>Balance:
+            if (wdamt>Balance):
                 print("Insufficient Funds!")
             else:
                 Balance -= wdamt
                 print("Withdrawal Successful. Your New Balance is {:,.2f}".format(Balance))
 
-        elif choice == 4:
+        elif (choice == 4):
             print("Thank You For Using Our ATM!")
             break
         
@@ -43,7 +43,7 @@ Kindly Enter Your Choice:
         
         choice = int(input("Enter Your Choice: "))
     
-    if choice<1 or choice>4:
+    if (choice<1 or choice>4):
         print("Please Enter a Valid Choice")
 
 else:
